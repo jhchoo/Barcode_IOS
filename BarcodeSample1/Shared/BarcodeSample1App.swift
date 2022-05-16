df38a2de5282a05c929fc3d7e31f437cd3a3cdbf
jhchoo <jhchoo.kr@gmail.com>
2022-05-16 13:47:21 +0900

    환경세팅완료 v1

---
+++ BarcodeSample1/Shared/BarcodeSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeSample1App.swift
+//  Shared
+//
+//  Created by jae hwan choo on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
